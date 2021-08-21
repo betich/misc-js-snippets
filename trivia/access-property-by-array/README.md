118498e76f692843e24fac4a674815f6025eb290
betich <betich365@gmail.com>
2021-08-22 03:37:11 +0700

    :sparkles: add access property by array

---
+++ trivia/access-property-by-array/README.md
@@ -0,0 +1,5 @@
+# Access Nested Properties by Array
+
+Access a nested object property through an array
+
+<a href="index.js" target="_blank"><img src="https://img.shields.io/badge/-Source-9633ff" alt="Source"></a> <a href="/README.md" target="_blank"><img src="https://img.shields.io/badge/-Back-f0f0f0" alt="back"></a>
---
+++ trivia/access-property-by-array/index.js
@@ -0,0 +1,38 @@
+// Access Nested Properties by Array
+// Access a nested object property through an array
+
+/**
+ * Accesses a nested property via an array
+ * @param {Object} obj Object to access the properties from
+ * @param {Array} query Array of properties to access
+ * @returns {*} Property of the object
+ */
+function getPropertyFromArray(obj, query) {
+  return query.reduce((acc, curr) => acc[curr], obj);
+}
+
+getPropertyFromArray(
+  {
+    a: {
+      b: {
+        c: "something",
+      },
+    },
+  },
+  ["a", "b", "c"]
+);
+
+// proof of concept
+const obj = {
+  a: {
+    b: {
+      c: {
+        d: "something",
+      },
+    },
+  },
+};
+
+const query = ["a", "b", "c"];
+
+query.reduce((acc, curr) => acc[curr], obj)["d"]; // something
